5f178068d344628742476fbd2d5f6a3a3ab0c7e2
Sefer Öztürk <sfroztrk99@gmail.com>
2023-07-27 16:22:17 +0300

    ui update

--- src/components/HarmonicExplorer.js
+++ src/components/HarmonicExplorer.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
+import { TextField, Button, Grid, Typography, Box } from "@mui/material";
 
 const HarmonicExplorer = () => {
   const [fundamentalFreq, setFundamentalFreq] = useState(440);
   const [harmonics, setHarmonics] = useState([
     { harmonic: 1, amplitude: 1, phaseAngle: 0 },
   ]);
-  const [cycles, setCycles] = useState(5);
+  const [cycles, setCycles] = useState(4);
 
   const changeFundamentalFrequency = (e) => {
     setFundamentalFreq(e.target.value);
@@ -43,19 +44,13 @@ const HarmonicExplorer = () => {
     setCycles(e.target.value);
   };
 
-  useEffect(() => {
-    if (fundamentalFreq === 0) {
-      alert("Frequency can not be 0. Frequency value set to 440");
-      setFundamentalFreq(440);
-    }
-  });
-
   const combinedSignal = () => {
     const time = [];
     const signal = [];
 
     const sampleRate = 50000;
     const duration = cycles / fundamentalFreq;
+
     const numSamples = sampleRate * duration;
 
     for (let i = 0; i < numSamples; i++) {
@@ -77,7 +72,7 @@ const HarmonicExplorer = () => {
   };
 
   useEffect(() => {
-    if (fundamentalFreq !== 0 && cycles > 0) {
+    if (fundamentalFreq != 0 && cycles > 0) {
       const combined = combinedSignal();
       setCombinedSignalData(combined);
 
@@ -90,6 +85,9 @@ const HarmonicExplorer = () => {
           range: [minSignalValue - 1, maxSignalValue + 1],
         },
       });
+    } else {
+      alert("Fundamental Frequency should be a non-zero number.");
+      setFundamentalFreq(440);
     }
   }, [fundamentalFreq, harmonics, cycles]);
 
@@ -113,10 +111,10 @@ const HarmonicExplorer = () => {
     const freq = parseFloat(getQueryParam("freq")) || 440;
     setFundamentalFreq(freq);
 
-    const cycleCount = parseFloat(getQueryParam("cycle")) || 5;
+    const cycleCount = parseFloat(getQueryParam("cycle")) || 4;
     setCycles(cycleCount);
 
-    const harmonicCount = parseFloat(getQueryParam("harmonic_number")) || 1;
+    const harmonicCount = parseFloat(getQueryParam("harmonics")) || 1;
     if (harmonicCount > 0) {
       const updatedHarmonics = [];
       for (let i = 1; i <= harmonicCount; i++) {
@@ -133,7 +131,7 @@ const HarmonicExplorer = () => {
 
     newUrlSearchParams.set("freq", fundamentalFreq.toString());
     newUrlSearchParams.set("cycle", cycles.toString());
-    newUrlSearchParams.set("harmonic_number", harmonics.length.toString());
+    newUrlSearchParams.set("harmonics", harmonics.length.toString());
 
     harmonics.forEach((harmonic, index) => {
       newUrlSearchParams.set(`amp${index + 1}`, harmonic.amplitude.toString());
@@ -143,113 +141,171 @@ const HarmonicExplorer = () => {
     window.history.replaceState({}, "", `?${newUrlSearchParams.toString()}`);
   }, [fundamentalFreq, harmonics, cycles]);
 
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const updateWindowDimensions = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowDimensions);
+
+    return () => {
+      window.removeEventListener("resize", updateWindowDimensions);
+    };
+  }, []);
+
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ flex: 1, marginRight: "20px", marginLeft: "50px" }}>
-        <h1>Harmonic Explorer</h1>
-        <div>
-          <label htmlFor="fundamentalFreq">Fundamental Frequency (Hz):</label>
-          <input
-            type="number"
-            id="fundamentalFreq"
-            value={fundamentalFreq}
-            onChange={changeFundamentalFrequency}
-            style={{ maxWidth: "50px" }}
-          />
-        </div>
-        <div>
-          <label htmlFor="cycles">Number of Cycles:</label>
-          <input
-            type="number"
-            id="cycles"
-            value={cycles}
-            onChange={changeCycles}
-            style={{ maxWidth: "50px" }}
-          />
-        </div>
-        <div>
-          <h2>Harmonics:</h2>
-          <div style={{ display: "flex" }}>
-            <div style={{ flex: 1 }}>
-              <button onClick={addHarmonic}>Add Harmonic</button>
-            </div>
-
-            <div style={{ flex: 1 }}>
-              <button onClick={() => removeLastHarmonic()}>
-                Remove Last Harmonic
-              </button>
-            </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Box p={2}>
+          <Typography variant="h1" fontSize="50px">
+            Harmonic Explorer
+          </Typography>
+          <div>
+            <TextField
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              type="number"
+              label="Fundamental Frequency (Hz)"
+              value={fundamentalFreq}
+              onChange={changeFundamentalFrequency}
+              style={{
+                maxWidth: "200px",
+                marginTop: "20px",
+              }}
+            />
+            <TextField
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              type="number"
+              label="Number of Cycles"
+              value={cycles}
+              onChange={changeCycles}
+              style={{
+                maxWidth: "200px",
+                marginTop: "20px",
+                marginLeft: "10px",
+              }}
+            />
           </div>
-
-          <ul>
-            {harmonics.map((harmonic, index) => (
-              <li key={harmonic.harmonic}>
-                <div>{`Harmonic ${harmonic.harmonic}: ${
-                  fundamentalFreq * harmonic.harmonic
-                } Hz`}</div>
-                <div>
-                  <label htmlFor={`amplitude-${harmonic.harmonic}`}>
-                    Amplitude:
-                  </label>
-                  <input
+          <div>
+            <Typography variant="h5" style={{ marginTop: "20px" }}>
+              Harmonics:
+            </Typography>
+            <Grid container spacing={1}>
+              <Grid item xs={12} style={{ marginTop: "20px" }}>
+                <Button variant="contained" onClick={addHarmonic}>
+                  Add Harmonic
+                </Button>
+                <Button
+                  variant="contained"
+                  style={{ marginLeft: "20px" }}
+                  onClick={removeLastHarmonic}
+                >
+                  Remove Last Harmonic
+                </Button>
+              </Grid>
+              {harmonics.map((harmonic, index) => (
+                <Grid
+                  item
+                  xs={12}
+                  key={harmonic.harmonic}
+                  style={{ marginTop: "10px" }}
+                >
+                  {harmonic.harmonic % 10 === 1 &&
+                  harmonic.harmonic % 100 !== 11 ? (
+                    <Typography>
+                      {harmonic.harmonic}st Harmonic Frequency:{" "}
+                      {fundamentalFreq * harmonic.harmonic} Hz
+                    </Typography>
+                  ) : harmonic.harmonic % 10 === 2 &&
+                    harmonic.harmonic % 100 !== 12 ? (
+                    <Typography>
+                      {harmonic.harmonic}nd Harmonic Frequency:{" "}
+                      {fundamentalFreq * harmonic.harmonic} Hz
+                    </Typography>
+                  ) : harmonic.harmonic % 10 === 3 &&
+                    harmonic.harmonic % 100 !== 13 ? (
+                    <Typography>
+                      {harmonic.harmonic}rd Harmonic Frequency:{" "}
+                      {fundamentalFreq * harmonic.harmonic} Hz
+                    </Typography>
+                  ) : (
+                    <Typography>
+                      {harmonic.harmonic}th Harmonic Frequency:{" "}
+                      {fundamentalFreq * harmonic.harmonic} Hz
+                    </Typography>
+                  )}
+                  <TextField
                     type="number"
-                    id={`amplitude-${harmonic.harmonic}`}
+                    label="Amplitude"
+                    size="small"
                     value={harmonic.amplitude}
-                    step={0.01}
-                    min={0}
+                    inputProps={{ step: 0.01, min: 0 }}
                     onChange={(e) =>
                       changeAmplitude(index, parseFloat(e.target.value))
                     }
-                    style={{ maxWidth: "50px" }}
+                    style={{ maxWidth: "150px", marginTop: "10px" }}
                   />
-                </div>
-                <div>
-                  <label htmlFor={`phase-${harmonic.harmonic}`}>
-                    Phase Angle (degrees):
-                  </label>
-                  <input
+                  <TextField
                     type="number"
-                    id={`phase-${harmonic.harmonic}`}
+                    label="Phase Angle (degrees)"
+                    size="small"
                     value={(harmonic.phaseAngle * 180) / Math.PI}
-                    step={0.1}
-                    min={0}
-                    max={360}
+                    inputProps={{ step: 0.1, min: 0, max: 360 }}
                     onChange={(e) =>
                       changePhaseAngle(index, parseFloat(e.target.value))
                     }
-                    style={{ maxWidth: "50px" }}
+                    style={{
+                      minWidth: "150px",
+                      marginTop: "10px",
+                      marginLeft: "20px",
+                    }}
                   />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <div style={{ flex: 1 }}>
-        <Plot
-          data={[
-            {
-              type: "scatter",
-              mode: "line",
-              x: combinedSignalData.time,
-              y: combinedSignalData.signal,
-            },
-          ]}
-          layout={{
-            width: 1000,
-            height: 500,
-            title: "Combined Signal",
-            xaxis: {
-              title: "Time (seconds)",
-            },
-            yaxis: {
-              title: "Amplitude",
-            },
-          }}
-        />
-      </div>
-    </div>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Box p={2}>
+          <Plot
+            data={[
+              {
+                type: "scatter",
+                mode: "line",
+                x: combinedSignalData.time,
+                y: combinedSignalData.signal,
+              },
+            ]}
+            layout={{
+              // width: windowSize.width * 0.6,
+              // height: windowSize.height * 0.6,
+              title: "Combined Signal",
+              xaxis: {
+                title: "Time (seconds)",
+              },
+              yaxis: {
+                title: "Amplitude",
+              },
+              autosize: true,
+            }}
+          />
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
