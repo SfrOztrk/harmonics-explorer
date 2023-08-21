72415723510c1e93030dc2f9dcd4c08c63388d12
Sefer Öztürk <sfroztrk99@gmail.com>
2023-08-21 11:40:43 +0300

    copy button props update

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -2,25 +2,31 @@ import React, { useState } from "react";
 import copy from "clipboard-copy";
 import { Button } from "@mui/material";
 
-const CopyButton = ({text, style, startIcon}) => {
-    const [copied, setCopied] = useState(false);
+const CopyButton = (props) => {
+  const [copied, setCopied] = useState(false);
 
-    const handleCopy = () => {
-        copy(text);
-        setCopied(true);
+  const handleCopy = () => {
+    copy(props.text);
+    setCopied(true);
 
-        setTimeout(() => {
-            setCopied(false);
-        }, 2000);
-    };
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
 
-    return (
-        <div>
-            <Button color="inherit" variant="outlined" onClick={handleCopy} startIcon={startIcon} style={style}>
-                {copied ? 'Copied' : 'Copy URL'}
-            </Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button
+        color="inherit"
+        variant="outlined"
+        onClick={handleCopy}
+        startIcon={props.startIcon}
+        style={props.style}
+      >
+        {copied ? "Copied" : "Copy URL"}
+      </Button>
+    </div>
+  );
 };
 
 export default CopyButton;
